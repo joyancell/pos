7e10916531dbdef9389c6a550613f3f832c9e7f4
joy ancell <joy.ancell30@gmail.com>
2022-03-03 11:44:28 +0800

    pertama

---
+++ db/pos.sql
@@ -0,0 +1,382 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Mar 2022 pada 11.31
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `barang_id` varchar(15) NOT NULL,
+  `barang_nama` varchar(150) DEFAULT NULL,
+  `barang_satuan` varchar(30) DEFAULT NULL,
+  `barang_harpok` double DEFAULT NULL,
+  `barang_harjul` double DEFAULT NULL,
+  `barang_harjul_grosir` double DEFAULT NULL,
+  `barang_stok` int(11) DEFAULT 0,
+  `barang_min_stok` int(11) DEFAULT 0,
+  `barang_tgl_input` timestamp NULL DEFAULT current_timestamp(),
+  `barang_tgl_last_update` datetime DEFAULT NULL,
+  `barang_kategori_id` int(11) DEFAULT NULL,
+  `barang_user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`barang_id`, `barang_nama`, `barang_satuan`, `barang_harpok`, `barang_harjul`, `barang_harjul_grosir`, `barang_stok`, `barang_min_stok`, `barang_tgl_input`, `barang_tgl_last_update`, `barang_kategori_id`, `barang_user_id`) VALUES
+('BR000001', 'tes barang', 'Kotak', 10000, 12000, 11000, 68, 10, '2022-01-18 04:31:30', NULL, 40, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_beli`
+--
+
+CREATE TABLE `tbl_beli` (
+  `beli_nofak` varchar(15) DEFAULT NULL,
+  `beli_tanggal` date DEFAULT NULL,
+  `beli_suplier_id` int(11) DEFAULT NULL,
+  `beli_user_id` int(11) DEFAULT NULL,
+  `beli_kode` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_beli`
+--
+
+INSERT INTO `tbl_beli` (`beli_nofak`, `beli_tanggal`, `beli_suplier_id`, `beli_user_id`, `beli_kode`) VALUES
+('1234', '2022-03-03', 2, 1, 'BL030322000001'),
+('1234', '2022-01-18', 1, 1, 'BL180122000001');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_detail_beli`
+--
+
+CREATE TABLE `tbl_detail_beli` (
+  `d_beli_id` int(11) NOT NULL,
+  `d_beli_nofak` varchar(15) DEFAULT NULL,
+  `d_beli_barang_id` varchar(15) DEFAULT NULL,
+  `d_beli_harga` double DEFAULT NULL,
+  `d_beli_jumlah` int(11) DEFAULT NULL,
+  `d_beli_total` double DEFAULT NULL,
+  `d_beli_kode` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_detail_beli`
+--
+
+INSERT INTO `tbl_detail_beli` (`d_beli_id`, `d_beli_nofak`, `d_beli_barang_id`, `d_beli_harga`, `d_beli_jumlah`, `d_beli_total`, `d_beli_kode`) VALUES
+(1, '1234', 'BR000001', 10000, 10, 100000, 'BL180122000001'),
+(2, '1234', 'BR000001', 10000, 10, 100000, 'BL030322000001');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_detail_jual`
+--
+
+CREATE TABLE `tbl_detail_jual` (
+  `d_jual_id` int(11) NOT NULL,
+  `d_jual_nofak` varchar(15) DEFAULT NULL,
+  `d_jual_barang_id` varchar(15) DEFAULT NULL,
+  `d_jual_barang_nama` varchar(150) DEFAULT NULL,
+  `d_jual_barang_satuan` varchar(30) DEFAULT NULL,
+  `d_jual_barang_harpok` double DEFAULT NULL,
+  `d_jual_barang_harjul` double DEFAULT NULL,
+  `d_jual_qty` int(11) DEFAULT NULL,
+  `d_jual_diskon` double DEFAULT NULL,
+  `d_jual_total` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_detail_jual`
+--
+
+INSERT INTO `tbl_detail_jual` (`d_jual_id`, `d_jual_nofak`, `d_jual_barang_id`, `d_jual_barang_nama`, `d_jual_barang_satuan`, `d_jual_barang_harpok`, `d_jual_barang_harjul`, `d_jual_qty`, `d_jual_diskon`, `d_jual_total`) VALUES
+(26, '180122000001', 'BR000001', 'tes barang', 'Kotak', 10000, 12000, 1, 0, 12000),
+(27, '180122000002', 'BR000001', 'tes barang', 'Kotak', 10000, 11000, 1, 0, 11000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_jual`
+--
+
+CREATE TABLE `tbl_jual` (
+  `jual_nofak` varchar(15) NOT NULL,
+  `jual_tanggal` timestamp NULL DEFAULT current_timestamp(),
+  `jual_total` double DEFAULT NULL,
+  `jual_jml_uang` double DEFAULT NULL,
+  `jual_kembalian` double DEFAULT NULL,
+  `jual_user_id` int(11) DEFAULT NULL,
+  `jual_keterangan` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_jual`
+--
+
+INSERT INTO `tbl_jual` (`jual_nofak`, `jual_tanggal`, `jual_total`, `jual_jml_uang`, `jual_kembalian`, `jual_user_id`, `jual_keterangan`) VALUES
+('180122000001', '2022-01-18 05:22:02', 12000, 50000, 38000, 1, 'eceran'),
+('180122000002', '2022-01-18 05:59:39', 11000, 20000, 9000, 1, 'grosir');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori_nama` varchar(35) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`kategori_id`, `kategori_nama`) VALUES
+(40, 'tes kategoriz');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_retur`
+--
+
+CREATE TABLE `tbl_retur` (
+  `retur_id` int(11) NOT NULL,
+  `retur_tanggal` timestamp NULL DEFAULT current_timestamp(),
+  `retur_barang_id` varchar(15) DEFAULT NULL,
+  `retur_barang_nama` varchar(150) DEFAULT NULL,
+  `retur_barang_satuan` varchar(30) DEFAULT NULL,
+  `retur_harjul` double DEFAULT NULL,
+  `retur_qty` int(11) DEFAULT NULL,
+  `retur_subtotal` double DEFAULT NULL,
+  `retur_keterangan` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_suplier`
+--
+
+CREATE TABLE `tbl_suplier` (
+  `suplier_id` int(11) NOT NULL,
+  `suplier_nama` varchar(35) DEFAULT NULL,
+  `suplier_alamat` varchar(60) DEFAULT NULL,
+  `suplier_notelp` varchar(20) DEFAULT NULL,
+  `suplier_email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_suplier`
+--
+
+INSERT INTO `tbl_suplier` (`suplier_id`, `suplier_nama`, `suplier_alamat`, `suplier_notelp`, `suplier_email`) VALUES
+(1, 'Toko  Ancell', 'tes alamat', '082144545220', 'joy.ancell30@gmail.com'),
+(2, 'testing suplier', 'testing alamat', '0982144545229', 'joy.ancell30@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL,
+  `user_nama` varchar(35) DEFAULT NULL,
+  `user_username` varchar(30) DEFAULT NULL,
+  `user_password` varchar(35) DEFAULT NULL,
+  `user_level` varchar(2) DEFAULT NULL,
+  `user_status` varchar(2) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `user_nama`, `user_username`, `user_password`, `user_level`, `user_status`) VALUES
+(1, 'ancell', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1', '1'),
+(2, 'Kasir', 'kasir', '827ccb0eea8a706c4c34a16891f84e7b', '2', '1'),
+(3, 'testing', 'testing', 'e10adc3949ba59abbe56e057f20f883e', '2', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`barang_id`),
+  ADD KEY `barang_user_id` (`barang_user_id`),
+  ADD KEY `barang_kategori_id` (`barang_kategori_id`);
+
+--
+-- Indeks untuk tabel `tbl_beli`
+--
+ALTER TABLE `tbl_beli`
+  ADD PRIMARY KEY (`beli_kode`),
+  ADD KEY `beli_user_id` (`beli_user_id`),
+  ADD KEY `beli_suplier_id` (`beli_suplier_id`),
+  ADD KEY `beli_id` (`beli_kode`);
+
+--
+-- Indeks untuk tabel `tbl_detail_beli`
+--
+ALTER TABLE `tbl_detail_beli`
+  ADD PRIMARY KEY (`d_beli_id`),
+  ADD KEY `d_beli_barang_id` (`d_beli_barang_id`),
+  ADD KEY `d_beli_nofak` (`d_beli_nofak`),
+  ADD KEY `d_beli_kode` (`d_beli_kode`);
+
+--
+-- Indeks untuk tabel `tbl_detail_jual`
+--
+ALTER TABLE `tbl_detail_jual`
+  ADD PRIMARY KEY (`d_jual_id`),
+  ADD KEY `d_jual_barang_id` (`d_jual_barang_id`),
+  ADD KEY `d_jual_nofak` (`d_jual_nofak`);
+
+--
+-- Indeks untuk tabel `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  ADD PRIMARY KEY (`jual_nofak`),
+  ADD KEY `jual_user_id` (`jual_user_id`);
+
+--
+-- Indeks untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indeks untuk tabel `tbl_retur`
+--
+ALTER TABLE `tbl_retur`
+  ADD PRIMARY KEY (`retur_id`);
+
+--
+-- Indeks untuk tabel `tbl_suplier`
+--
+ALTER TABLE `tbl_suplier`
+  ADD PRIMARY KEY (`suplier_id`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_detail_beli`
+--
+ALTER TABLE `tbl_detail_beli`
+  MODIFY `d_beli_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_detail_jual`
+--
+ALTER TABLE `tbl_detail_jual`
+  MODIFY `d_jual_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_retur`
+--
+ALTER TABLE `tbl_retur`
+  MODIFY `retur_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_suplier`
+--
+ALTER TABLE `tbl_suplier`
+  MODIFY `suplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD CONSTRAINT `tbl_barang_ibfk_1` FOREIGN KEY (`barang_user_id`) REFERENCES `tbl_user` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_barang_ibfk_2` FOREIGN KEY (`barang_kategori_id`) REFERENCES `tbl_kategori` (`kategori_id`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_beli`
+--
+ALTER TABLE `tbl_beli`
+  ADD CONSTRAINT `tbl_beli_ibfk_1` FOREIGN KEY (`beli_user_id`) REFERENCES `tbl_user` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_beli_ibfk_2` FOREIGN KEY (`beli_suplier_id`) REFERENCES `tbl_suplier` (`suplier_id`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_detail_beli`
+--
+ALTER TABLE `tbl_detail_beli`
+  ADD CONSTRAINT `tbl_detail_beli_ibfk_1` FOREIGN KEY (`d_beli_barang_id`) REFERENCES `tbl_barang` (`barang_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_detail_beli_ibfk_2` FOREIGN KEY (`d_beli_kode`) REFERENCES `tbl_beli` (`beli_kode`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_detail_jual`
+--
+ALTER TABLE `tbl_detail_jual`
+  ADD CONSTRAINT `tbl_detail_jual_ibfk_1` FOREIGN KEY (`d_jual_barang_id`) REFERENCES `tbl_barang` (`barang_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_detail_jual_ibfk_2` FOREIGN KEY (`d_jual_nofak`) REFERENCES `tbl_jual` (`jual_nofak`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  ADD CONSTRAINT `tbl_jual_ibfk_1` FOREIGN KEY (`jual_user_id`) REFERENCES `tbl_user` (`user_id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
